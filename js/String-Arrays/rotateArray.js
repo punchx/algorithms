3272d9a3e51125c1729b61eebc7cfaa5a84d889e
punch <glowdesign.md@gmail.com>
2020-02-10 17:29:15 +0200

    rotate array es6 style

---
+++ js/String-Arrays/rotateArray.js
@@ -0,0 +1,10 @@
+// Rotate an array of n elements to the right by k steps.
+// For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+// How many different ways do you know to solve this problem?
+
+let arrayRotate01 = (arr, k) => {
+  let dIndex = arr.length - k;
+  return [...arr.slice(dIndex), ...arr.slice(0, dIndex)];
+}
+
+console.log(arrayRotate01([1, 2, 3, 4, 5, 6, 7], 5));
